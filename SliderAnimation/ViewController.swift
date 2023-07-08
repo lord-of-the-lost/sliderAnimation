d689d4fe4c646b0addb62f838259127b1033fba0
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-08 23:50:43 +0300

    Initial Commit

---
+++ SliderAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SliderAnimation
+//
+//  Created by Николай Игнатов on 08.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
